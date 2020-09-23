9f73b5bca39917c585130189ebdc9bdb91966837
bing-dong <643252149@qq.com>
2020-09-23 23:26:07 +0800

    lab1 primes

---
+++ user/primes.c
@@ -0,0 +1,66 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+void sieve(int pd[2]) {
+    int p;
+    if (read(pd[0], &p, sizeof(p)) != 0) {
+        printf("prime %d\n", p);
+    }
+    else {
+        close(pd[0]);
+        exit(0);
+    }
+
+    int pd2[2];
+    if (pipe(pd2) != 0) {
+        printf("pipe created failed\n");
+        printf("fd: %d\n", dup(0));
+    }
+
+    if (fork() == 0) {
+        close(pd[0]);
+        close(pd2[1]);
+        sieve(pd2);
+    }
+    else {
+        close(pd2[0]);
+        //read(pd[0], &p, sizeof(p));
+        //printf("prime %d\n", p);
+        int i;
+        while (read(pd[0], &i, sizeof(i)) != 0) {
+            if (i % p) {
+                write(pd2[1], &i, sizeof(i));
+            }
+        }
+        close(pd[0]);
+        close(pd2[1]);
+
+        wait(0);
+    }
+}
+
+int main()
+{
+    int pd[2];
+    if (pipe(pd) != 0) {
+        printf("pipe created failed\n");
+        printf("fd: %d\n", dup(0));
+    }
+
+    if (fork() == 0) {
+        close(pd[1]);
+        sieve(pd);
+    }
+    else {
+        close(pd[0]);
+        for (int i = 2; i <= 35; i++) {
+            write(pd[1], &i, sizeof(i));
+        }
+        //虽然exit时会自动关闭，但不关闭子进程就会阻塞在read不会退出
+        //所以在这里需要主动关闭
+        close(pd[1]);
+        wait(0);
+    }
+
+    exit(0);
+}
