4f5788d97a3732ea935e25c4dad16a2d14bc8cff
bing-dong <643252149@qq.com>
2020-09-28 12:22:53 +0800

    lab1 find

--- user/find.c
+++ user/find.c
@@ -1,58 +1,68 @@
 #include "kernel/types.h"
-#include "kernel/stat.h"
 #include "user/user.h"
+#include "kernel/stat.h"
 #include "kernel/fs.h"
 
-/**
- * Version 0: strcmp didn't judge return code 
- * Version 1: use 'return' instead of 'exit()' in main , which caused error. 'Return' is a language primitive however 'exit' is a system call.
- */
-void find(char *p,char *f){
-    char buf[512],*i;
+void find(char *dir, char *filename) 
+{
     int fd;
-    struct dirent de;
+    char dirbuf[512];
     struct stat st;
-    if((fd=open(p,0))<0){
-        printf("Failed to open dir=%s \n",p);
-        exit(1);
+    struct dirent diren;
+
+    if ((fd = open(dir, 0)) < 0) {
+        printf("open dir failed\n");
+        exit(-1);
     }
-    if((fstat(fd,&st))<0){
-        printf("Failed to get dir stat=%s \n",p);
+
+    if (fstat(fd, &st) < 0) {
+        printf("stat failed\n");
+        exit(-1);
+    }
+
+    if (st.type != T_DIR) {
         close(fd);
         exit(1);
     }
-    if (st.type!=T_DIR){
-        close(fd);
-        return ;
-    }
-    strcpy(buf,p);
-    i=buf+strlen(buf);
-    *i++='/';
-    while(read(fd,&de,sizeof de)==sizeof de){
-        if(de.inum==0 || strcmp(".",de.name) ==0 || strcmp("..",de.name)==0){
+
+    strcpy(dirbuf, dir);
+    int buf_idx = strlen(dir);
+    dirbuf[buf_idx++] = '/';
+
+    //read目录，每次返回一个目录项
+    while (read(fd, &diren, sizeof(diren)) == sizeof(diren)) {
+        //diren.inum不可忽视
+        if (diren.inum == 0 || strcmp(".", diren.name) == 0 || strcmp("..", diren.name) == 0) {
             continue;
         }
-        memmove(i,de.name,DIRSIZ);
-        i[DIRSIZ]=0;
-        if(stat(buf,&st)<0){
-            printf("Failed to stat file=%s",buf);
-            exit(1);
+        memcpy(dirbuf+buf_idx, diren.name, DIRSIZ);
+        buf_idx += DIRSIZ;
+        dirbuf[buf_idx] = '\0';
+
+        if (stat(dirbuf, &st) < 0) {
+            printf("stat error\n");
+            exit(-1);
         }
-        if(st.type==T_FILE){
-            if(strcmp(de.name,f)==0){
-                printf("%s\n",buf);
-            }
-        }else if(st.type==T_DIR){
-            find(buf,f);
+
+        if (st.type == T_FILE && strcmp(filename, diren.name) == 0) {
+            printf("%s\n", dirbuf);
         }
+        else if (st.type == T_DIR) {
+            find(dirbuf, filename);
+        }
+        buf_idx -= DIRSIZ;
     }
     close(fd);
 }
-int main(int argc,char* argv[]){
-    if(argc!=3){
-        printf("Usage: <path> <regex>\n");
-        exit(1);
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3) {
+        printf("usage: find dir filename\n");
+        exit(-1);
     }
-    find(argv[1],argv[2]);
+
+    find(argv[1], argv[2]);
+
     exit(0);
 }
